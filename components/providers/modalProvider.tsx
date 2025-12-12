c5466a7c0f4b02340eccc1f465a802f5e4dd39f0
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-12-12 20:09:59 +0530

    modal provider

--- components/providers/modalProvider.tsx
+++ components/providers/modalProvider.tsx
@@ -2,6 +2,9 @@
 
 import { CreateServerModal } from "@/components/modals/createServerModal";
 import { useEffect, useState } from "react";
+import { InviteModal } from "../modals/inviteModal";
+import { EditServerModal } from "../modals/editServerModal";
+import { MembersModal } from "../modals/membersModal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -17,6 +20,9 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />;
+      <InviteModal />;
+      <EditServerModal />;
+      <MembersModal />;
     </>
   );
 };
