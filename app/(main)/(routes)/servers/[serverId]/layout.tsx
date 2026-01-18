d3d69b1ecbf0ac17432c8c40ddc58dbddb9aca96
Aayushman-nvm <aayushman.jha001@gmail.com>
2026-01-18 23:33:42 +0530

    main route layout

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -38,7 +38,7 @@ const ServerIdLayout = async ({
       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
         <ServerSidebar serverId={serverId} />
       </div>
-      <main className="h-full md:pl-60">{children}</main>
+      <main className="h-full md:pl-[280px]">{children}</main>
     </div>
   );
 };
