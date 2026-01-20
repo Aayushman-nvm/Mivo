e3b9afc471b5bbbd1e4dbf40290f6507ec7e9948
Aayushman-nvm <aayushman.jha001@gmail.com>
2026-01-20 16:58:30 +0530

    file upload

--- components/fileUpload.tsx
+++ components/fileUpload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
@@ -26,6 +26,30 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
         <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm hover:bg-rose-600 transition"
+          type="button"
+          aria-label="Remove image"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  } 
+  
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm hover:bg-rose-600 transition"
           type="button"
           aria-label="Remove image"
         >
