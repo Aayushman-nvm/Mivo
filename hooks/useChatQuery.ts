9ee65e8c6957e0976a103297565073babe3948f5
Aayushman-nvm <aayushman.jha001@gmail.com>
2026-02-12 19:23:07 +0530

    use chat query

---
+++ hooks/useChatQuery.ts
@@ -0,0 +1,58 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import qs from "query-string";
+import { useSocket } from "@/components/providers/socketProvider";
+
+interface ChatQueryProps {
+  queryKey: string;
+  apiUrl: string;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+}
+
+const useChatQuery = ({
+  queryKey,
+  apiUrl,
+  paramKey,
+  paramValue,
+}: ChatQueryProps) => {
+  const { isConnected } = useSocket();
+
+  const fetchMessages = async ({
+    pageParam,
+  }: {
+    pageParam?: string;
+  }) => {
+    const url = qs.stringifyUrl(
+      {
+        url: apiUrl,
+        query: {
+          cursor: pageParam,
+          [paramKey]: paramValue,
+        },
+      },
+      { skipNull: true }
+    );
+
+    const res = await fetch(url);
+    return res.json();
+  };
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      queryKey: [queryKey, paramValue],
+      queryFn: fetchMessages,
+      initialPageParam: undefined,
+      getNextPageParam: (lastPage) => lastPage?.nextCursor,
+      refetchInterval: isConnected ? false : 1000,
+    });
+
+  return {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  };
+};
+
+export default useChatQuery;
